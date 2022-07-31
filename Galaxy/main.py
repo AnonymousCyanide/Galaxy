c275575005eb3859ca15041e738ee5fc255c0dd0
Avim <avidubey2000@gmail.com>
2022-08-01 02:41:16 +0530

    Added vertical lines from perpective to base

--- Galaxy/main.py
+++ Galaxy/main.py
@@ -1,13 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
+from kivy.graphics.context_instructions import Color 
+from kivy.graphics.vertex_instructions import Line 
 
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
+    V_NB_LINES = 14
+    V_LINES_SPACING = .1
+    vertical_lines = []
+    line = None
 
     def __init__(self, **kwargs):
         super(MainWidget , self).__init__(**kwargs)
+        self.init_vertical_lines()
         #print('Init func ',self.width , self.height)
 
 
@@ -17,7 +24,9 @@ class MainWidget(Widget):
         pass
     # Called when size is changed
     def on_size(self, widget , parent):
-       print('Size func ',self.width , self.height)
+        self.update_vertical_line()
+        print('Size func ',self.width , self.height)
+       
        # COmmeting out cause doing in galaxy.kv file
       # self.perspective_point_x = self.width/2
       # self.perspective_point_y = self.height * 0.75
@@ -30,6 +39,23 @@ class MainWidget(Widget):
     # Called when perspective_point_y is changed as it is a property
     def on_perspective_point_y(self,widget,value):
         print('Py :' , value)
+
+    # Is called in the __init__
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1,1,1)
+            for i in range(0,self.V_NB_LINES):
+                self.vertical_lines.append(Line())
+
+    def update_vertical_line(self):
+        center_x = self.width/2
+        offset = -int(self.V_NB_LINES/2)
+        spacing = self.V_LINES_SPACING * self.width
+        for i in range(0,self.V_NB_LINES):
+            linex = center_x + offset * spacing
+            self.vertical_lines[i].points = [self.perspective_point_x ,self.perspective_point_y , linex , 0]
+            offset += 1
+
 
 class GalaxyApp(App):
     pass
