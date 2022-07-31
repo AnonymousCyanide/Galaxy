08ae5056ac7ce61b4a11f296e92232f20846ae8d
Avim <avidubey2000@gmail.com>
2022-08-01 02:15:01 +0530

    Added perspective cordinates and made akv file

--- Galaxy/main.py
+++ Galaxy/main.py
@@ -1,9 +1,38 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
+
 class MainWidget(Widget):
-    pass
+    perspective_point_x = NumericProperty(0)
+    perspective_point_y = NumericProperty(0)
+
+    def __init__(self, **kwargs):
+        super(MainWidget , self).__init__(**kwargs)
+        #print('Init func ',self.width , self.height)
+
+
+    # Called when widget is attached to the Main app
+    def on_parent(self, widget , parent):
+        #print('Parent func ',self.width , self.height)
+        pass
+    # Called when size is changed
+    def on_size(self, widget , parent):
+       print('Size func ',self.width , self.height)
+       # COmmeting out cause doing in galaxy.kv file
+      # self.perspective_point_x = self.width/2
+      # self.perspective_point_y = self.height * 0.75
+
+
+    # Called when perspective_point_x is changed as it is a property
+    def on_perspective_point_x(self,widget,value):
+        print('Px :' , value)
+    
+    # Called when perspective_point_y is changed as it is a property
+    def on_perspective_point_y(self,widget,value):
+        print('Py :' , value)
+
 class GalaxyApp(App):
     pass
 
-if __name__ == '__main__':
-    GalaxyApp().run()
+
+GalaxyApp().run()
